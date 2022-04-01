61584ddf421c1ef35260cb0150c0826ec45e1737
marridG <1687450542@qq.com>
2022-04-01 09:14:52 +0800

    statistical successful rate analysis

--- simulation/tests/test_encoding_v2.py
+++ simulation/tests/test_encoding_v2.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 from data.taffic_signs import TrafficSignsData
 from sign_boards import TrafficSignBoard
 from lidar_points import LiDARSampling
@@ -34,7 +36,11 @@ pt_sample_obj = LiDARSampling(canvas=sign_board_obj,
 sample_res, sample_res_loc = pt_sample_obj.sample_at_distance(dist=50)
 # print(sample_res)
 
-for _sample, _sample_loc in zip(sample_res, sample_res_loc):
+cnt_success, cnt_success_full, cnt_success_not_full_not_tol, cnt_success_tol, cnt_failure = 0, 0, 0, 0, 0
+# for _sample, _sample_loc in zip(sample_res, sample_res_loc):
+for _sample_idx in tqdm(range(0, len(sample_res), 10)):
+    _sample = sample_res[_sample_idx]
+    _sample_loc = sample_res_loc[_sample_idx]
     try:
         _sample_decoded, _sample_decoded_info = decode(
             sign_data_obj=data_obj,
@@ -43,8 +49,29 @@ for _sample, _sample_loc in zip(sample_res, sample_res_loc):
             vert_margin=utils.dist_2_margin(dist=distance, angle_resol=vert_angle_resol),
             height=height, width=width,
             tolerable=True)
-        print(_sample_decoded, _sample_decoded_info)
+        # print(_sample_decoded, _sample_decoded_info)
+        _is_suc = True
+        if _is_suc is True and _sample_decoded["category_1"] is not None:
+            _is_suc = (_sample_decoded["category_1"] == data_sample["encoding"]["category"])
+        if _is_suc is True and _sample_decoded["category_2"] is not None:
+            _is_suc = (_sample_decoded["category_2"] == data_sample["encoding"]["idx"])
+        if _is_suc is True and _sample_decoded["num"] is not None:
+            _is_suc = (_sample_decoded["num"] == data_sample["num"])
+        # update counts
+        if _is_suc is False:
+            cnt_failure += 1
+            continue
+        cnt_success += 1
+        if _sample_decoded_info["is_complete"] is True:
+            cnt_success_full += 1
+        if (_sample_decoded_info["is_complete"] is False) and _sample_decoded_info.get("is_tolerated") is None:
+            cnt_success_not_full_not_tol += 1
+        if _sample_decoded_info.get("is_tolerated") is True:
+            cnt_success_tol += 1
     except DecodeFailure as err:
-        print("Decode Failed for Sample Loc (%d,%d): %s" % (_sample_loc["hori"], _sample_loc["vert"], str(err)))
+        # print("Decode Failed for Sample Loc (%d,%d): %s" % (_sample_loc["hori"], _sample_loc["vert"], str(err)))
+        cnt_failure += 1
         continue
+print("[ALL] %d\n\t[SUCCESS] %d (full=%d, !full!tol=%d, tolerated=%d)\n\t[FAILURE] %d"
+      % (len(sample_res), cnt_success, cnt_success_full, cnt_success_not_full_not_tol, cnt_success_tol, cnt_failure))
 print()
