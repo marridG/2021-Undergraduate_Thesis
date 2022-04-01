37625f40ed641dc0419bab63d3ffb3073aecd425
marridG <1687450542@qq.com>
2022-04-01 09:15:04 +0800

    remove all codes in test.py

--- simulation/test.py
+++ simulation/test.py
@@ -1,15 +1,1 @@
-# from sign_boards import TrafficSignBoard
-# from visualization import VisualCanvas
-#
-# obj = TrafficSignBoard()
-# print(type(obj))
-# print(isinstance(obj, VisualCanvas))
 
-
-from data.taffic_signs import TrafficSignsData
-from encoding_v1 import encode
-
-obj = TrafficSignsData()
-s = obj.get_sample(fixed_or_family=1)
-print(s)
-print(encode.encode(sample=s))
