18a248a28e068676652846f1e269e9febd1ad01d
marridG <1687450542@qq.com>
2022-05-14 16:14:41 +0800

    in file lidar_resolution_analysis.py: change some settings for thesis writing

--- simulation/analysis/lidar_resolution_analysis.py
+++ simulation/analysis/lidar_resolution_analysis.py
@@ -120,8 +120,9 @@ def cal_max_line_resol_diff(_size, _resol, _dist, scale: str = "mm"):
     raise NotImplementedError
 
 
-sign_size_n_resol_vals = [(130., 0.1), (130., 0.2), (130., 0.4),
-                          (65. * np.sqrt(3), 0.33), (65. * np.sqrt(3), 2.)]  # in (centi-meters, degrees)
+# sign_size_n_resol_vals = [(130., 0.1), (130., 0.2), (130., 0.4),
+#                           (65. * np.sqrt(3), 0.33), (65. * np.sqrt(3), 2.)]  # in (centi-meters, degrees)
+sign_size_n_resol_vals = [(120., 0.1), (120, 0.33)]  # in (centi-meters, degrees)
 distance_vals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                  20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]  # in meters
 print("d_j\t\\omega_0\td_0\t=>\tj(2j+1)\tdelta\tLine Resol Diff\tResol")
