65701f5469f5429985654544411d3ec27f1cdb6f
marridG <1687450542@qq.com>
2022-06-11 15:37:17 +0800

    finish max distance calculation. further todo: best height:width ratio finder

--- design_analysis/max_dist_cal.py
+++ design_analysis/max_dist_cal.py
@@ -62,7 +62,8 @@ class MaxDistCal:
         return res
 
     def cal_dist(self, bar_hw_ratio: float,
-                 lv1_pt_cnt_vert: int, lv1_pt_cnt_hori: int, lv2_pt_cnt_vert: int, lv2_pt_cnt_hori: int) \
+                 lv1_pt_cnt_vert: int, lv1_pt_cnt_hori: int, lv2_pt_cnt_vert: int, lv2_pt_cnt_hori: int,
+                 print_res: bool = False) \
             -> Dict[str, Dict[str, Dict[str, float]]]:
         """
         calculate the maximum distance where lv-1 or lv-2 info can be 100% extracted
@@ -71,6 +72,7 @@ class MaxDistCal:
         :param lv1_pt_cnt_hori:             number of HORIZONTAL sample points required for lv-1 info
         :param lv2_pt_cnt_vert:             number of VERTICAL sample points required for lv-2 info
         :param lv2_pt_cnt_hori:             number of HORIZONTAL sample points required for lv-2 info
+        :param print_res:                   whether to print the calculated result max distances
         :return:                        maximum distance (in METERs) calculation results, as a <dict>:
                                         {
                                             shape_name: {
@@ -79,39 +81,53 @@ class MaxDistCal:
                                         }
         """
 
-        assert lv1_pt_cnt_hori < self.ENC_BAR_CNT_COL and 0 == self.ENC_BAR_CNT_COL % lv1_pt_cnt_hori
-        assert lv1_pt_cnt_vert < self.ENC_BAR_CNT_ROW and 0 == self.ENC_BAR_CNT_ROW % lv1_pt_cnt_vert
-        assert lv2_pt_cnt_hori < self.ENC_BAR_CNT_COL and 0 == self.ENC_BAR_CNT_COL % lv2_pt_cnt_hori
-        assert lv2_pt_cnt_vert < self.ENC_BAR_CNT_ROW and 0 == self.ENC_BAR_CNT_ROW % lv2_pt_cnt_vert
+        # assert lv1_pt_cnt_hori <= self.ENC_BAR_CNT_COL and 0 == self.ENC_BAR_CNT_COL % lv1_pt_cnt_hori
+        # assert lv1_pt_cnt_vert <= self.ENC_BAR_CNT_ROW and 0 == self.ENC_BAR_CNT_ROW % lv1_pt_cnt_vert
+        # assert lv2_pt_cnt_hori <= self.ENC_BAR_CNT_COL and 0 == self.ENC_BAR_CNT_COL % lv2_pt_cnt_hori
+        # assert lv2_pt_cnt_vert <= self.ENC_BAR_CNT_ROW and 0 == self.ENC_BAR_CNT_ROW % lv2_pt_cnt_vert
 
-        lv1_max_width_by_bar_cnt = self.ENC_BAR_CNT_COL // lv1_pt_cnt_hori
-        lv1_max_height_by_bar_cnt = self.ENC_BAR_CNT_ROW // lv1_pt_cnt_vert
-        lv2_max_width_by_bar_cnt = self.ENC_BAR_CNT_COL // lv2_pt_cnt_hori
-        lv2_max_height_by_bar_cnt = self.ENC_BAR_CNT_ROW // lv2_pt_cnt_vert
+        lv1_max_width_by_bar_cnt = self.ENC_BAR_CNT_COL / lv1_pt_cnt_hori
+        lv1_max_height_by_bar_cnt = self.ENC_BAR_CNT_ROW / lv1_pt_cnt_vert
+        lv2_max_width_by_bar_cnt = self.ENC_BAR_CNT_COL / lv2_pt_cnt_hori
+        lv2_max_height_by_bar_cnt = self.ENC_BAR_CNT_ROW / lv2_pt_cnt_vert
 
         res = {}
         for shape in ["rectangle", "circle", "triangle"]:
+            res[shape] = {}
             bar_height, bar_width = self._cal_bar_hw(bar_hw_ratio=bar_hw_ratio, shape=shape)
             # level 1 info
             dist_lv1_hori = self._cal_hori_dist_by_width(width=bar_width * lv1_max_width_by_bar_cnt) / 1000.  # m
             dist_lv1_vert = self._cal_vert_dist_by_height(height=bar_height * lv1_max_height_by_bar_cnt) / 1000.  # m
             dist_lv1 = min(dist_lv1_hori, dist_lv1_vert)
-            res[shape] = {"lv1": {"dist": dist_lv1, "hori": dist_lv1_hori, "vert": dist_lv1_vert}}
+            res[shape]["lv1"] = {"dist": dist_lv1, "hori": dist_lv1_hori, "vert": dist_lv1_vert}
             # lv2 info
-            dist_lv2_hori = self._cal_hori_dist_by_width(width=bar_width * lv2_max_width_by_bar_cnt) / 2000.  # m
-            dist_lv2_vert = self._cal_vert_dist_by_height(height=bar_height * lv2_max_height_by_bar_cnt) / 2000.  # m
+            dist_lv2_hori = self._cal_hori_dist_by_width(width=bar_width * lv2_max_width_by_bar_cnt) / 1000.  # m
+            dist_lv2_vert = self._cal_vert_dist_by_height(height=bar_height * lv2_max_height_by_bar_cnt) / 1000.  # m
             dist_lv2 = min(dist_lv2_hori, dist_lv2_vert)
-            res[shape] = {"lv2": {"dist": dist_lv2, "hori": dist_lv2_hori, "vert": dist_lv2_vert}}
+            res[shape]["lv2"] = {"dist": dist_lv2, "hori": dist_lv2_hori, "vert": dist_lv2_vert}
 
+        if print_res is True:
+            for shape, shape_dist in res.items():
+                for _lv, _lv_dist in shape_dist.items():
+                    print("%s [%s] = %.3fm (hori=%.3fm, vert=%.3fm)"
+                          % (shape[:3].upper(), _lv, _lv_dist["dist"], _lv_dist["hori"], _lv_dist["vert"]))
         return res
 
 
 if "__main__" == __name__:
-    # 2*4, 2 per bar
-    # 2*2, 2 per bar
-
-    # 2*5, 1 per bar
-    obj = MaxDistCal(enc_bar_cnt_col=10, enc_bar_cnt_row=2)
-    res = obj.cal_dist(bar_hw_ratio=3.3, lv1_pt_cnt_vert=2, lv1_pt_cnt_hori=5, lv2_pt_cnt_vert=2, lv2_pt_cnt_hori=10)
-    for shape, shape_res in res.items():
-        print("RECT [LV 2] = %f (hori=%f, vert=%f)" % (dist_lv2, dist_lv2_hori, dist_lv2_vert))  # todo
+    pass
+    # # 2*4, 2 per bar
+    # obj = MaxDistCal(enc_bar_cnt_col=8, enc_bar_cnt_row=2)
+    # _ = obj.cal_dist(bar_hw_ratio=3.3,
+    #                  lv1_pt_cnt_vert=2, lv1_pt_cnt_hori=8, lv2_pt_cnt_vert=2, lv2_pt_cnt_hori=16, print_res=True)
+    # print()
+    # # 2*5, 1 per bar
+    # obj = MaxDistCal(enc_bar_cnt_col=10, enc_bar_cnt_row=2)
+    # _ = obj.cal_dist(bar_hw_ratio=3.3,
+    #                  lv1_pt_cnt_vert=2, lv1_pt_cnt_hori=5, lv2_pt_cnt_vert=2, lv2_pt_cnt_hori=10, print_res=True)
+    # print()
+    # # 2*2, 2 per bar
+    # obj = MaxDistCal(enc_bar_cnt_col=8, enc_bar_cnt_row=2)
+    # _ = obj.cal_dist(bar_hw_ratio=3.3,
+    #                  lv1_pt_cnt_vert=2, lv1_pt_cnt_hori=4, lv2_pt_cnt_vert=2, lv2_pt_cnt_hori=16, print_res=True)
+    # print()
